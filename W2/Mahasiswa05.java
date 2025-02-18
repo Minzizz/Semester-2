158da40ec3be1ef21204d785b035da9270713dac
Minzizz <azizsudjud@gmail.com>
2025-02-18 09:10:55 +0700

    first commit

---
+++ W2/Mahasiswa05.java
@@ -0,0 +1,51 @@
+package W2;
+
+public class Mahasiswa05 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa05(){
+        
+    }
+
+    public Mahasiswa05(String nm, String nim, double ipk, String kls){
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
+    void tampilkanInformasi(){
+        System.out.println("Nama: "+nama);
+        System.out.println("NIM: "+nim);
+        System.out.println("IPK: "+ipk);
+        System.out.println("Kelas: "+kelas);
+    }
+
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+
+    void updateIPK(double ipkBaru){
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja(){
+        if(ipk>=3.5){
+            return "Kinerja sangat baik";
+        }else if (ipk >= 3.0){
+            return "Kinerja baik";
+        }else if (ipk >= 2.0){
+            return "Kinerja cukup";
+        }else {
+            return "kinerja kurang";
+        }
+    }
+
+    
+
+    
+
+}
---
+++ W2/MahasiswaMain05.java
@@ -0,0 +1,20 @@
+package W2;
+
+public class MahasiswaMain05 {
+    public static void main(String[] args) {
+        Mahasiswa05 mhs1 = new Mahasiswa05();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2441720171";
+        mhs1.kelas = "Si 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampilkanInformasi();
+
+        Mahasiswa05 mhs2 = new Mahasiswa05("Annisa Nabila", "2141720160", 3.25, "Ti2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
+    }
+}
